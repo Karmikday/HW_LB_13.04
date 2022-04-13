63f2e7d796f461fffb3db462f8c97403762cc8f2
Tata_kn18 <karmikday@gmail.com>
2022-04-13 19:39:05 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 int main()
 {
-
+	srand(time(0));
+	int size;
+	cin >> size;
+	int* arr = new int[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+		cout << arr[i] << endl;
+	}
 }
