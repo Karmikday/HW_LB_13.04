e5c863596dd33ab86f32e8421816ea9896f86173
Tata_kn18 <karmikday@gmail.com>
2022-04-13 19:25:09 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 int main()
 {
-
+	int* p_1 = new int();
+	int* p_2 = new int();
+	cin >> *p_1;
+	cin >> *p_2;
+	cout << "----------------------------------------" << endl;
+	swap(*p_1, *p_2);
+	cout << *p_1 << endl;
+	cout << *p_2 << endl;
+	system("pause");
 }
