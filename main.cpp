46c51c2b4604741e43696e5ebdafd6b661b22580
Tata_kn18 <karmikday@gmail.com>
2022-04-13 19:28:27 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 int main()
 {
+	int* p_1 = new int();
+	int* p_2 = new int();
+	cout << "Vvdetite 1 znachenie->";
+	cin >> *p_1;
+	cout << "Vvdetite 2 znachenie->";
+	cin >> *p_2;
+	cout << "Answer->" << *p_1 + *p_2 << endl;
 
 }
