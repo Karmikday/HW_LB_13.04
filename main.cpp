238e33965e56be4355d2855a7a4072e10741febc
Tata_kn18 <karmikday@gmail.com>
2022-04-13 19:04:25 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main()
 {
+	int* p_1 = new int();
+	int* p_2 = new int();
+	cout << "Vvdetite 1 znachenie->";
+	cin >> *p_1;
+	cout << "Vvdetite 2 znachenie->";
+	cin >> *p_2;
+	if (*p_1 > *p_2)
+	{
+		cout << *p_1 << endl;
+	}
+	if (*p_1 < *p_2)
+	{
+		cout << *p_2 << endl;
+	}
 
 }
