c54cbe8d26a1a9e0945b062187a113e9faf3776d
Tata_kn18 <karmikday@gmail.com>
2022-04-16 14:11:56 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-using namespace std;
+#include <time.h>
+using namespace std;	
 int main()
 {
-
+	srand(time(0));
+	int size;
+	cout << "Size->";
+	cin >> size;
+	int* arr1 = new int[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		arr1[i] = rand() % 20;
+		cout << arr1[i] << endl;
+	}
+	cout << "------------------------------------" << endl;
+	for (int i = 0; i <= size / 2; ++i)
+	{
+		swap(arr1[i], arr1[size - 1 - i]);
+	}
+	for (int i = 0; i < size; ++i)
+	{
+		cout << arr1[i] << endl;
+	}
+	
 }
